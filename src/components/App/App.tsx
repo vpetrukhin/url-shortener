a7d3d38d5507c74c1cfc62d4ca845f7686b10b7b
Vasya <vpetruin@inbox.ru>
2022-04-18 10:13:23 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      worked!
+      <p className={styles.appText}>kdnvcjcnk</p>
+    </div>
+  );
+}
+
+export default App;
