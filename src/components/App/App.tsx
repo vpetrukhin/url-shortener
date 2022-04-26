e334b2746de2922767ef2ca4b7e5b5fd6c2fdb52
Vasya <vpetruin@inbox.ru>
2022-04-26 16:59:19 +0300

    Убрана верхняя секция

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,9 @@
-import { FormSection, MainSection } from '../../sections/';
+import { FormSection } from '../../sections/';
 import styles from './App.module.scss';
 
 export const App = () => {
   return (
     <div className={styles.app}>
-      <MainSection />
       <FormSection />
     </div>
   );
